fcf620fea755bfe54ffa94e9c8183c1f5d823a70
Pedram Hajesmaeeli <pedram@Pedrams-MacBook-Air.local>
2025-07-24 09:26:18 +0200

    Add rendered.py

---
+++ renderer.py
@@ -0,0 +1,35 @@
+import json
+from enum import Enum
+import jsonschema
+
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+class Template(Enum):
+    DEFAULT_RESUME = "resume.html"
+
+class ResumeDataValidationError(Exception):
+    pass
+
+class ResumeRenderer:
+    _template_dir = "templates"
+    _schema_path = "cv.schema.json"
+
+    def __init__(self):
+        self.env = Environment(
+            loader=FileSystemLoader(self._template_dir),
+            autoescape=select_autoescape(['html', 'xml'])
+        )
+
+    def render(self, resume_data, template: Template):
+        self._validate_resume_data(resume_data)
+        template = self.env.get_template(template.value)
+        return template.render(**resume_data)
+
+    def _validate_resume_data(self, resume_data):
+        with open(self._schema_path, 'r') as schema_file:
+            schema = json.load(schema_file)
+
+        try:
+            jsonschema.validate(instance=resume_data, schema=schema)
+        except jsonschema.exceptions.ValidationError as e:
+            raise ResumeDataValidationError("Invalid resume data") from e
