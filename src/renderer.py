e510674d91ec0aed2e46bb1502351c1d5688b308
Pedram Hajesmaeeli <pedram@Pedrams-MacBook-Air.local>
2025-07-28 12:10:18 +0200

    Add support for markdown

--- src/renderer.py
+++ src/renderer.py
@@ -5,7 +5,9 @@ from pathlib import Path
 from typing import Any, Dict, cast
 
 import jsonschema
+import markdown  # type: ignore[import]
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from markupsafe import Markup
 
 
 class ResumeTemplate(Enum):
@@ -31,7 +33,8 @@ class ResumeRenderer:
     def render_resume(self, resume_data: Dict[str, Any], resume_template: ResumeTemplate) -> str:
         self._validate_resume_data(resume_data)
         resolved_template = self.env.get_template(resume_template.value)
-        return resolved_template.render(**resume_data)  # type: ignore[no-any-return]
+        resume_data_with_processed_markdown = self._markdown_to_html_for_dict(resume_data)
+        return resolved_template.render(**resume_data_with_processed_markdown)  # type: ignore[no-any-return]
 
     def render_error(self, error_message: str) -> str:
         template = self.env.get_template("error.html")
@@ -47,3 +50,27 @@ class ResumeRenderer:
     def _schema(self) -> Dict[str, Any]:
         with open(self._schema_path, "r") as schema_file:
             return cast(Dict[str, Any], json.load(schema_file))
+
+    def _markdown_to_html(self, markdown_text: str) -> Markup:
+        """Convert Markdown text to HTML."""
+        html = markdown.markdown(markdown_text.strip(), extensions=["fenced_code", "tables"])
+        if html.startswith("<p>") and html.endswith("</p>"):
+            html = html[3:-4]
+        return Markup(html)
+
+    def _markdown_to_html_for_dict(self, data: Dict[str, Any]) -> Dict[str, Any]:
+        """Recursively convert Markdown strings in a dictionary to HTML."""
+        for key, value in data.items():
+            data[key] = self._turn_to_html_recursively(value)
+        return data
+
+    def _turn_to_html_recursively(self, data: Any) -> Any:
+        if isinstance(data, str):
+            return self._markdown_to_html(data)
+        if isinstance(data, dict):
+            return {key: self._turn_to_html_recursively(value) for key, value in data.items()}
+        if isinstance(data, list):
+            return [self._turn_to_html_recursively(item) for item in data]
+        if isinstance(data, tuple):
+            return tuple(self._turn_to_html_recursively(item) for item in data)
+        return data
