a60cd25b70a2a68d7ef7e79c16e57285fc661445
Pedram Hajesmaeeli <pedram@Pedrams-MacBook-Air.local>
2025-07-28 11:06:33 +0200

    Show jsonschema in the error message

--- src/renderer.py
+++ src/renderer.py
@@ -1,7 +1,8 @@
 import json
 from enum import Enum
+from functools import cached_property
 from pathlib import Path
-from typing import Any, Dict
+from typing import Any, Dict, cast
 
 import jsonschema
 from jinja2 import Environment, FileSystemLoader, select_autoescape
@@ -34,13 +35,15 @@ class ResumeRenderer:
 
     def render_error(self, error_message: str) -> str:
         template = self.env.get_template("error.html")
-        return template.render(error_message=error_message)  # type: ignore[no-any-return]
+        return template.render(error_message=error_message, json_schema=self._schema)  # type: ignore[no-any-return]
 
     def _validate_resume_data(self, resume_data: Dict[str, Any]) -> None:
-        with open(self._schema_path, "r") as schema_file:
-            schema = json.load(schema_file)
-
         try:
-            jsonschema.validate(instance=resume_data, schema=schema)
+            jsonschema.validate(instance=resume_data, schema=self._schema)
         except jsonschema.exceptions.ValidationError as e:
             raise ResumeDataValidationError(e) from e
+
+    @cached_property
+    def _schema(self) -> Dict[str, Any]:
+        with open(self._schema_path, "r") as schema_file:
+            return cast(Dict[str, Any], json.load(schema_file))
